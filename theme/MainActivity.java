de5d0282a6fcab3cfff6eceb7a6a528ead972ec5
scissna <94951533+scissna@users.noreply.github.com>
2022-11-07 12:29:16 +0530

    Add files via upload

---
+++ theme/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.theme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.co.in/"));
+                startActivity(i);
+            }
+        });
+    }
+}
